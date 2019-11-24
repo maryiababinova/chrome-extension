a2e3d50d95963b0b33cf147b5678fe8fbf38c9bc
Maryia Miller <maryiamiller@gmail.com>
2019-11-24 13:00:19 -0500

    added comments

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
+//declring the localStorage variable
 let storage = window.localStorage;
 
 //checking the category of the item
 const body = document.getElementsByTagName('body');
 let utag_data = body[0].children[0].innerHTML;
+//extracting the category keyword
 const idx = utag_data.indexOf('page_breadcrumb');
 let category = utag_data
   .slice(idx)
@@ -14,12 +16,12 @@ let category = utag_data
   .toString()
   .replace(/[^a-z-]/g, '');
 
-//checking for the CSE_Challenge value in the local storage
-//and updating the affinity score
+//checking for the CSE_Challenge value in the localStorage
 let affinity = storage.getItem('CSE_Challenge');
-if (affinity) {
-  affinity = JSON.parse(affinity);
-} else {
+//if there's CSE_Challenge value, pasrsing it
+if (affinity) affinity = JSON.parse(affinity);
+//if not, assigning a new affinity object
+else {
   affinity = {
     'womens-clothing': 0,
     'mens-clothing': 0,
@@ -28,14 +30,16 @@ if (affinity) {
     beauty: 0,
   };
 }
+
+//updating the affinity score for the item and storing it in localStorage
 affinity[category]++;
 storage.setItem('CSE_Challenge', JSON.stringify(affinity));
 
-//checking if the item was added to cart and updating the score accordingly
+//gettin the 'add to cart' button
 const addToCart = document.getElementsByClassName(
   'js-add-to-cart o-button-basket c-product-add-to-cart__button'
 );
-
+//checking if the item was added to cart and updating the score accordingly
 addToCart[0].addEventListener('click', () => {
   affinity[category] += 3;
   storage.setItem('CSE_Challenge', JSON.stringify(affinity));
